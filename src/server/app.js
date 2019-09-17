6d4a389d04cad36042fd3ec31ff673899e1f8db1
Richie Redizm <richieredizm@gmail.com>
2019-09-17 17:05:32 +0100

    create voting results against age chart in js devided up by gender

--- src/server/app.js
+++ src/server/app.js
@@ -2,25 +2,28 @@
  * Simple backend for generating survey results from JSON input
  */
 
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-
-const app = express();
-
+const express = require('express')
+const cors = require('cors')
+const fs = require('fs')
+const app = express()
+const port = 3456
 /**
  * Enable basic CORS for backend
  */
-app.use(cors());
+app.use(cors())
 
 app.get('/survey', (req, res) => {
-    try {
-        res.status(200).send(JSON.parse(fs.readFileSync('survey-sample.json', 'utf8')));
-    } catch(error) {
-        res.status(500).send({
-            error: error.message
-        })
-    }
-});
+  try {
+    res
+      .status(200)
+      .send(JSON.parse(fs.readFileSync('survey-sample.json', 'utf8')))
+  } catch (error) {
+    res.status(500).send({
+      error: error.message
+    })
+  }
+})
 
-app.listen(3456);
+app.use(express.static('public'))
+
+app.listen(port)
